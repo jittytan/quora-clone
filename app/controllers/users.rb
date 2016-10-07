a9360c933b161d794da3b43f954b5ba0814d1efc
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-10-07 14:03:50 +0800

    vr 2

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -9,7 +9,7 @@ end
 
 get '/logout' do
 
-	byebug
+	# byebug
 
 	session[:user_id] = nil
 	erb :"static/index"
@@ -18,14 +18,14 @@ end
 
 # in users_controller.rb
 post '/signup' do
-  user = User.new(user_name: params[:user][:user_name], email: params[:user][:email], password: params[:user][:password])
+  user = User.new(user_name: params[:new_user][:user_name], email: params[:new_user][:email], password: params[:new_user][:password])
   user.save
   if user.save
     redirect "/homepage" # what should happen if the user is save?
   else
-  	@flash_msg = {info: "Not successful"}
-		return @flash_msg
-		erb :"static/homepage"
+  	@flash_msg = user.errors.full_messages#{info: "Not successful"}
+		# return @flash_msg
+		erb :"static/index"
     # what should happen if the user keyed in invalid data?
   end
 end  
@@ -33,19 +33,34 @@ end
 
 post '/login' do
 
-	if User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+	@user = User.find_by(email: params[:user][:email])
+	if @user && @user.authenticate(params[:user][:password])
+		# .try(:authenticate, params[:user][:password])
 		# In fact, the act of "logging in", is simply the the act of having your user id stored inside the session hash. 
 		# The act of "logging out" is simply the act of your user id being removed from the session hash. 
 		# The session hash is most commonly used to store info like a user's id, 
 		# which the web application will use to know who is the "current user" and show that user the appropriate information 
-		session[:user_id] = User.find_by(email: params[:user][:email])[:id]
+		session[:user_id] = @user.id
 		redirect "/homepage"
+	
 
+	else 
+		@msg = "Invalid information"
+		erb :"static/index"
 	end
   # apply a authentication method to check if a user has entered a valid email and password
   # if a user has successfully been authenticated, you can assign the current user id to a session
   
 end
+
+
+get '/users/:id' do
+	erb :"static/profile"
+end
+
+
+
+
 
 post '/logout' do
 
@@ -55,4 +70,5 @@ post '/logout' do
   # kill a session when a user choses to logout, for example assign nil to a session
   # redirect to the appropriate page
 end
+
 
