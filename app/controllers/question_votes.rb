141acea12c4fd1b0ddc8e681624bd530c351b228
Tan Jit SIang <joetjsjoe@hotmail.com>
2016-10-18 12:21:47 +0800

    finished i hope

--- app/controllers/question_votes.rb
+++ app/controllers/question_votes.rb
@@ -1,6 +1,6 @@
-post '/up_votes/:question_id' do
+post '/questions/:question_id/up_vote' do
 # post '/questions/:question_id/up_vote'
-	@vote = current_user.question_votes.find_by(user_id: current_user.id, vote_type: true) if current_user
+	@vote = current_user.question_votes.find_by(user_id: current_user.id, vote_type: true, question_id: params[:question_id])
 	if @vote
 		@vote.destroy #click it again to remove the upvote
 	else
@@ -14,10 +14,12 @@ post '/up_votes/:question_id' do
 		 # return @abc.to_json
 
 	@count = QuestionVote.where(question_id: params[:question_id], vote_type: true).count
+	@id = params[:question_id]
 
-	@array = [@save, @count]
+	@array = [@save, @count, @id]
 	# @hash[save] = @save
 	# @hash[count] = @count
+
 
 	return  @array.to_json
 	# # redirect '/homepage'
@@ -26,23 +28,29 @@ post '/up_votes/:question_id' do
 
 end
 
-post '/down_votes/:question_id' do
+post '/questions/:question_id/down_vote' do
 
 
-	if current_user.question_votes.find_by(vote_type: false) == nil
+	@downvote = current_user.question_votes.find_by(user_id: current_user.id, vote_type: false, question_id: params[:question_id])
 
-		@down_vote = current_user.question_votes.new(user_id: current_user.id, question_id:params[:question_id], vote_type: false)
-		@down_vote.save
-	else 
-
-		@down_vote = current_user.question_votes.find_by(user_id: current_user.id, vote_type: false)
-		@down_vote.destroy
-	end
-
-	if @down_vote.save || @downn_vote.destroy
-		redirect '/homepage'
+	if @downvote 
+		@downvote.destroy
 	else
-		@flash_msg = @down_vote.errors.full_messages
+		@down_vote = current_user.question_votes.new(question_id:params[:question_id], vote_type: false)
+		@down_vote.save
+		@savedown = true
 	end
+
+	@countdown = QuestionVote.where(question_id: params[:question_id], vote_type: false).count
+	@id = params[:question_id]
+
+	@array_down = [@savedown, @countdown, @id]
+	return @array_down.to_json
+
+	# if @down_vote.save || @downn_vote.destroy
+	# 	redirect '/homepage'
+	# else
+	# 	@flash_msg = @down_vote.errors.full_messages
+	# end
 
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
 
-	$("#upvote_form").submit(function(e) {
-		console.log( $(this).serialize() );
-		console.log( $(this).attr("method") );
-		console.log( $(this).attr("action") );
+	$(".upvote_form").submit(function(e) {
+		// console.log( $(this).serialize() );
+		// console.log( $(this).attr("method") );
+		// console.log( $(this).attr("action") );
 		e.preventDefault();
 
 		$.ajax({
+
+
 			type: $(this).attr("method"),
 			url: $(this).attr("action"),
-			data: $(this).serialize(),
+			data: $(this).serialize(),	
 // 			//Serialize a form to a query string that could be sent
 // 			//to a server in an Ajax request.
 // 			//http[://api.jquery.com/serialize/
@@ -20,29 +22,71 @@ $(document).ready(function() {
 
 				console.log(object);
 
-				$('#bla').html('Up Votes: ' + object[1]);
+				$('#bla_' + object[2]).html('Up Votes: ' + object[1]);
 				if (object[0] === true) { // the vote has been casted by user and will change the value of box to indicate to remove vote
 					console.log("good")
-				$('#upvote_box').empty().append(
-					'<input type=submit value="Remove up vote">');}
+					$('#upvote_box_' + object[2]).html(
+						'<button type=submit class="btn btn-primary"><span class="glyphicon glyphicon-remove"> Remove up vote</span></button>');}
+					// '<input type=submit value="Remove up vote">');}
 				else {
 					console.log("nice")
-					$('#upvote_box').empty().append(
-					'<input type=submit value="Up vote">');
-					};
-					
-
+					$('#upvote_box_' + object[2]).html(
+						'<button type=submit class="btn btn-primary"><span class="glyphicon glyphicon-thumbs-up"> Up vote</span></button>');
+					// '<input type=submit value="Up vote">');
+				};
 				// 	$('#bla').html(count);
-				// $('#upvote_box').empty().append(
+				// $('#upvote_box').html(
 				// 	'<input type=submit value="Vote down" >'
-				// }
 			}
 			// end of success
-			
-// end of ajax
 		});
+		// end of ajax(upvote)
 	});
+	// end of submit
+
+	$(".downvote_form").submit(function(e) {
+		e.preventDefault();
+
+		$.ajax({
+			type: $(this).attr("method"),
+			url: $(this).attr("action"),
+			data: $(this).serialize(),
+			dataType: "json",
+
+			success: function(array){
+				console.log(array);
+
+				$('#dvt_' + array[2]).html(' | Down Votes: ' + array[1]);
+				if (array[0] ===true) {
+					console.log("voted, now change to remove")
+					$('#downvote_box_' + array[2]).html(
+						'<button type=submit class="btn btn-primary"><span class="glyphicon glyphicon-remove"> Remove your Down vote</span></button>'
+					);}
+        else {
+        	console.log("have not voted, change to add")
+        	$('#downvote_box_' + array[2]).html(
+        		'<button type=submit class="btn btn-primary"><span class="glyphicon glyphicon-thumbs-down"> Down vote</span></button>'
+        		);
+        };
+			}
+			// end of success
+		});
+		// end of ajax(downvote)
+	});
+	// end of submit
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
+// end of ready
 
-// $("#results").empty().append(myHtml);
